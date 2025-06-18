6290f326352b68c4ff577dd56f438c3f84b79a2f
Falsky <steralk.mn@yandex.com>
2025-06-18 12:17:42 +0300

    change platform and locate

--- config/platform.php
+++ config/platform.php
@@ -29,7 +29,7 @@ return [
      |
      */
 
-    'prefix' => env('PLATFORM_PREFIX', '/admin'),
+    'prefix' => env('PLATFORM_PREFIX', '/veles'),
 
     /*
      |--------------------------------------------------------------------------
@@ -160,8 +160,8 @@ return [
      */
 
     'template' => [
-        'header' => '',
-        'footer' => '',
+        'header' => 'brand.header',
+        'footer' => 'brand.footer',
     ],
 
     /*
